abf67e9e03bfdcece883711e64d3fba8f2fe8953
cristopher3454O <cristo654cabl3@gmail.com>
2026-09-18 11:04:54 -0600

    Ejericicios simples

---
+++ Basic/03-Ejercicios.js
@@ -0,0 +1,26 @@
+// 1. Escribe un comentario en una línea
+//Cristopher Alexander Breceda Lerma
+
+// 2. Escribe un comentario en varias líneas
+/* Yo soy Cristopher Alexander Breceda Lerma
+estudiante de Ing en Software */
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+var nombre = "Cristopher"
+let edad = 18
+const altura = 1.80
+
+// 4. Imprime por consola el valor de todas las variables
+console.log(nombre)
+console.log(edad)
+console.log(altura)
+
+// 5. Imprime por consola el tipo de todas las variables
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+nombre = "PEDRO"
+edad = 56
+//altura = 2.4
+console.log(nombre)
+console.log(edad)
+console.log(altura)
